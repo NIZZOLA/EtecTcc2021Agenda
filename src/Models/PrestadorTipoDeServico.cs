d836ecff61cd8e3a417c48fa96445d37ce9251bc
nizzola <marcio@nizzola.com.br>
2021-11-16 20:24:20 -0300

    Inclusão de classe faltante

---
+++ src/Models/PrestadorTipoDeServico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TCC.Agenda.Models
+{
+
+    [Table("PrestadorTipoDeServico")]
+    public class PrestadorTipoDeServico
+    {
+        [Key]
+        public int PrestadorTipoDeServicoId { get; set; }
+
+        [ForeignKey("Prestador")]
+        public Guid PrestadorId { get; set; }
+        public PrestadorModel Prestador { get; set; }
+
+        [ForeignKey("TipoDeServico")]
+        public Guid TipoDeServicoId { get; set; }
+        public TipoDeServicoModel TipoDeServico { get; set; }
+
+        public decimal PercentualDeParticipacao { get; set; }
+    }
+}
