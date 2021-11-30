fd1d658ef087bc6d37dc996911cc0ecef4f046a0
ANA RAFAELA SANTOS DA SILVA <ana.silva4439@etec.sp.gov.br>
2021-11-30 19:33:31 -0300

    implementação de usuário e prestador

---
+++ src/Migrations/20211123223313_Login_23.11.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+namespace TCC.Agenda.Migrations
+{
+    public partial class Login_2311 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
